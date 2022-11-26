290bc6aa989bcf0779ddb8587b5efdb32b97d6d0
Hendri Muji W <hendrimuji31@gmail.com>
2022-11-26 22:02:20 +0700

    Add script FoodSpawner
    Pilih makanan di Ruang Makan muncul secara random.

--- Game13 - Healthy Kids Fun/Assets/Script/Spawner/FoodSpawner.cs
+++ Game13 - Healthy Kids Fun/Assets/Script/Spawner/FoodSpawner.cs
@@ -1,18 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class FoodSpawner : MonoBehaviour
 {
+    [SerializeField] GameObject[] prefabs;
+    // [SerializeField] GameObject[] post;
+    [SerializeField] GameObject post;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        int spawn = Random.Range(0, prefabs.Length);
+        // int pos = Random.Range(0, post.Length);
+
+        GameObject newImage = Instantiate(prefabs[spawn], transform.position, Quaternion.identity);
+        newImage.transform.SetParent(post.transform, true);
+        // newImage.transform.SetParent(post[pos].transform, true);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
