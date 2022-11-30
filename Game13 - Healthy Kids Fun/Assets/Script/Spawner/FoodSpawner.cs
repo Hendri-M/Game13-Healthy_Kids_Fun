19ecfb3cc75840cd8f671f37e55178a549644ecd
Hendri Muji W <hendrimuji31@gmail.com>
2022-11-30 16:53:21 +0700

    Add Launch Box and Fixed Scene's

--- Game13 - Healthy Kids Fun/Assets/Script/Spawner/FoodSpawner.cs
+++ Game13 - Healthy Kids Fun/Assets/Script/Spawner/FoodSpawner.cs
@@ -7,17 +7,16 @@ public class FoodSpawner : MonoBehaviour
 {
     [SerializeField] GameObject[] prefabs;
     // [SerializeField] GameObject[] post;
-    [SerializeField] GameObject post;
+    [SerializeField] GameObject foodPosition;
 
     // Start is called before the first frame update
     void Start()
     {
         int spawn = Random.Range(0, prefabs.Length);
-        // int pos = Random.Range(0, post.Length);
 
         GameObject newImage = Instantiate(prefabs[spawn], transform.position, Quaternion.identity);
-        newImage.transform.SetParent(post.transform, true);
-        // newImage.transform.SetParent(post[pos].transform, true);
+        newImage.transform.SetParent(foodPosition.transform, true);
+       
     }
 
 }
